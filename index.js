4505076285055f4d20a280af0862c9268f362acd
Rajeswari R <127644829+Rajiii77@users.noreply.github.com>
2024-10-01 16:48:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function rolling(){
     for(let i=0; i<dice; i++){
         let values=Math.floor(Math.random() * 6) + 1;
         val.push(values);
-        img.push(`<img src="dice_img/${values}.png">`)
+        img.push(`<img src="${values}.png">`)
        
 
     }
